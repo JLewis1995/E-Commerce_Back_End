78c11e958dee867e53271c0f56f947d6823dcb14
Jacob Lewis <jacobllewis@outlook.com>
2022-03-29 15:08:30 -0500

    finished routes. Testing complete. comments and readme to be added.

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -57,7 +57,7 @@ router.put('/:id', async (req, res) => {
     if (!category) {
       res.status(404).json({ message: `No Category found with matching Id` })
     }
-    res.status(200).json(category)
+    res.status(200).json(req.body)
   } catch (err) {
     res.status(500).json(err);
   }
@@ -70,7 +70,7 @@ router.delete('/:id', async (req, res) => {
     if (!category) {
       res.status(4040).json({ message: `No Category found with matching Id` })
     }
-    res.status(200).json(category);
+    res.status(200).json(`Category ${req.params.id} deleted`);
   } catch (err) {
     res.status(500).json(err);
   }
